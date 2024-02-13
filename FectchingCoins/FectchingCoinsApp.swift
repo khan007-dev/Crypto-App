d6297ac5c52efddc4f3705cdd0255bf6e24ac1c2
abdulwariskhan <hellokhan.ch@gmail.com>
2024-02-13 23:43:30 +0100

    Initial Commit

---
+++ FectchingCoins/FectchingCoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FectchingCoinsApp.swift
+//  FectchingCoins
+//
+//  Created by Khan on 13.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct FectchingCoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
