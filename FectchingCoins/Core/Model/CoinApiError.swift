6603deb7d35b39a3b6202df1ec7913c7b44fe89b
abdulwariskhan <hellokhan.ch@gmail.com>
2024-02-18 07:57:50 +0100

    Implemented Custom Error Handling & Retain Cycles methodology

--- FectchingCoins/Core/Model/CoinApiError.swift
+++ FectchingCoins/Core/Model/CoinApiError.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum CoinApiError: Error {
+    
+    case invalidData
+    case jsonParsingFailure
+    case requestFailed(description: String)
+    case invalidStatusCode(statusCode: Int)
+    case unknownError(error: Error)
+    
+    var customDescription: String {
+        
+        switch self {
+        case .invalidData: return "Invalid data"
+        case .jsonParsingFailure: return "Failed to parse JSON"
+        case let .requestFailed(description) : return "Request Failed: \(description)"
+        case let .invalidStatusCode(statusCode): return "Invalid Status Code: \(statusCode)"
+        case let .unknownError(error): return "An Unknown error Occured \(error.localizedDescription)"
+        }
+    }
+}
--- FectchingCoins/Core/Service/CoinDataService.swift
+++ FectchingCoins/Core/Service/CoinDataService.swift
@@ -8,65 +8,84 @@
 import Foundation
 class CoinDataService {
     let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=chf&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=24h&locale=en"
-    func fetchCoinsWithResult(completion: @escaping(Result<[Coin], Error>) -> Void) {
+    func fetchCoinsWithResult(completion: @escaping(Result<[Coin], CoinApiError>) -> Void) {
         guard let url = URL(string: urlString) else {return}
         
         URLSession.shared.dataTask(with: url) { data, response, error  in
             
              if let error = error {
-                 completion(.failure(error))
+                 completion(.failure(.unknownError(error: error)))
                 return
             }
-            guard let data = data else {return}
             
-            guard  let coins = try? JSONDecoder().decode([Coin].self, from: data) else {
-                print("DEBUG: Failed to Decode coins")
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(.requestFailed(description: "Request Failed")))
+                           return
+                       }
+            
+            guard httpResponse.statusCode == 200  else {
+                             
+                completion(.failure(.invalidStatusCode(statusCode: httpResponse.statusCode)))
+                              return
+                          }
+            
+            guard let data = data else {
+                
+                completion(.failure(.invalidData))
                 return}
-        
-            completion(.success(coins))
+            
+            do {
+                let coins = try JSONDecoder().decode([Coin].self, from: data)
+                completion(.success(coins))
+            }
+            catch {
+                print("DEBUG: Failed to decode with error \(error)")
+                completion(.failure(.jsonParsingFailure))
+            }
+    
             
         }.resume()
     }
     
   
-    func fetchPrice(coin: String, completion: @escaping (Double) -> Void) {
+//    func fetchPrice(coin: String, completion: @escaping (Double) -> Void) {
+//        
+//        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
+//        
+//        guard let url = URL(string: urlString) else { return}
+//        
+//        URLSession.shared.dataTask(with: url) { data, response, error in
+//            
+//            DispatchQueue.main.async {
+//                
+//                if let error = error {
+//                    print("DEBUG: Failed with Error \(error.localizedDescription)")
+//                    //   self.errorMessage = error.localizedDescription
+//                    return
+//                }
+//                
+//                guard let httpResponse = response as? HTTPURLResponse else {
+//                    //    self.errorMessage = "Bad HTTP Response"
+//                    return
+//                }
+//                
+//                guard httpResponse.statusCode == 200  else {
+//                    //   self.errorMessage = "failed to fetch with status code \(httpResponse.statusCode)"
+//                    return
+//                }
+//                
+//                guard let data = data else {return}
+//                guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {return}
+//                
+//                guard let value = jsonObject[coin] as? [String: Int] else {return}
+//                guard let price = value["usd"] else {return}
+//                
+//                print("Price is \(price)")
+//                completion(Double(price))
+//                
+//            }
+//        }.resume()
+//      
         
-        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
-        
-        guard let url = URL(string: urlString) else { return}
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            
-            DispatchQueue.main.async {
-                
-                if let error = error {
-                    print("DEBUG: Failed with Error \(error.localizedDescription)")
-                    //   self.errorMessage = error.localizedDescription
-                    return
-                }
-                
-                guard let httpResponse = response as? HTTPURLResponse else {
-                    //    self.errorMessage = "Bad HTTP Response"
-                    return
-                }
-                
-                guard httpResponse.statusCode == 200  else {
-                    //   self.errorMessage = "failed to fetch with status code \(httpResponse.statusCode)"
-                    return
-                }
-                
-                guard let data = data else {return}
-                guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {return}
-                
-                guard let value = jsonObject[coin] as? [String: Int] else {return}
-                guard let price = value["usd"] else {return}
-                
-                print("Price is \(price)")
-                completion(Double(price))
-                
-            }
-        }.resume()
-      
-        
-    }
+    
 }
