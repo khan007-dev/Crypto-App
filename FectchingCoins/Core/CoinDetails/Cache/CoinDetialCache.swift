8fd29347260e573307c6f39bbaef51fd7a4df005
abdulwariskhan <hellokhan.ch@gmail.com>
2024-02-20 15:49:18 +0100

    implemented Cached Coin Detials  from API

---
+++ FectchingCoins/Core/CoinDetails/Cache/CoinDetialCache.swift
@@ -0,0 +1,27 @@
+//
+//  CoinDetialCahce.swift
+//  FectchingCoins
+//
+//  Created by Khan on 20.02.2024.
+//
+
+import Foundation
+class CoinDetialCahce {
+    
+    static let shared = CoinDetialCahce()
+    
+    private let cache = NSCache<NSString, NSData>()
+    
+    private init() {
+        
+    }
+    func set(_ coinDetials: CoinDetial, forKey key: String) {
+     
+        guard let data = try? JSONEncoder().encode(coinDetials) else { return}
+        cache.setObject(data as NSData, forKey: key as NSString)
+    }
+    func get(forKey key: String) -> CoinDetial? {
+        guard let data = cache.object(forKey: key as NSString) as Data? else { return nil }
+        return try? JSONDecoder().decode(CoinDetial.self, from: data)
+    }
+}
