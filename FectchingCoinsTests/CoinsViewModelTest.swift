876fac208565db94841e7027392385e0e37ab9ac
abdulwariskhan <hellokhan.ch@gmail.com>
2024-02-22 11:25:44 +0100

    Tested Coin View Model

--- FectchingCoinsTests/CoinsViewModelTest.swift
+++ FectchingCoinsTests/CoinsViewModelTest.swift
@@ -5,4 +5,27 @@
 //  Created by Khan on 22.02.2024.
 //
 
-import Foundation
+import XCTest
+@testable import FectchingCoins
+class CoinsViewModelTest: XCTestCase {
+    
+    func testInit(){
+        
+        let service = MockCoinService()
+        let viewModel = CoinsViewModel(service: service)
+        
+        XCTAssertNotNil(viewModel, "The view model should not nil")
+    }
+    
+    func testSuccessFulCoinsFetch()  async {
+        let service = MockCoinService()
+        let viewModel = CoinsViewModel(service: service)
+        
+        await viewModel.fetchCoins()
+        
+        XCTAssertTrue(viewModel.coins.count > 0)
+        XCTAssertEqual(viewModel.coins.count, 1)
+    }
+    
+   
+}
